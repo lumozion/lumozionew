889c6e323eb70f8dad1aad2d421c79ef5349efa9
Ashwin Asthana <lumozyon@gmail.com>
2025-06-15 12:26:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${outfit.variable} ${mulish.variable} ${inter.variable}`}>
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="lumozionew/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="lumozionew/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="lumozionew/favicon-16x16.png">
-        <link rel="manifest" href="lumozionew/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+        <link rel="manifest" href="./site.webmanifest">
       </head>
       <body className="font-mulish antialiased">{children}</body>
     </html>
